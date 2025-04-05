0319e49ed74db99929cf870715bde70cd1c35058
Camila Scaglioni <camila.pia.scaglioni@gmail.com>
2025-04-05 08:52:09 -0300

    se quita el header de las pantallas

--- gymApp/app/_layout.tsx
+++ gymApp/app/_layout.tsx
@@ -5,8 +5,12 @@ import Toast from "react-native-toast-message";
 export default function Layout() {
   return (
     <>
-      <Stack />
-      <StatusBar style="dark" />
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="machines" options={{ headerShown: false }} />
+        <Stack.Screen name="activities" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
       <Toast />
     </>
   );
